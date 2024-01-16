c6f2bbf8662e16db25061d068df2aa2b03ed9dde
Aarush Acharya <92685647+Aarush-Acharya@users.noreply.github.com>
2024-01-16 21:44:45 +0530

    feat: chaneg name

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bit Burst',
+      title: 'Cash Burst',
       theme: ThemeData(primarySwatch: Colors.amber, fontFamily: 'Cinzel'),
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
